05bb6c8deffb915857abb3f8cf0ab27550cc5c6f
Jan Kaiponen <itsjan@users.noreply.github.com>
2020-12-21 12:17:21 +0200

    add topnav
    Added to all pages. Replaced old footer an all pages.

--- coursework/final-project/dist/js/main.js
+++ coursework/final-project/dist/js/main.js
@@ -30,4 +30,15 @@ function toggleMenu() {
   }
 }
 
+// https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleResponsiveMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 console.log("Hello from the main.js script");
